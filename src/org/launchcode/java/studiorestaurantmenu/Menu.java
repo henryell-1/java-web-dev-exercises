b15e3dedffee2b3de447b5ea751ff74076ff50ec
Henry Ell <henrydell4793@gmail.com>
2021-09-21 10:58:35 -0500

    This is the second commit for the Studio on the 16th.

--- src/org/launchcode/java/studiorestaurantmenu/Menu.java
+++ src/org/launchcode/java/studiorestaurantmenu/Menu.java
@@ -1,16 +1,22 @@
 package org.launchcode.java.studiorestaurantmenu;
 
 
+import jdk.internal.jimage.ImageStrings;
+import jdk.internal.jimage.ImageStringsReader;
+
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.Date;
 
 public class Menu{
     private Date lastUpdated;
     private ArrayList<MenuItems> items;
+    private ImageStringsReader item;
 
-    public Menu(Date d, ArrayList<MenuItems> i){
+
+    public Menu(Date d, ArrayList<MenuItems> item){
         this.lastUpdated = d;
-        this.items = i;
+        this.items = item;
     }
 
     public void  setLastUpdated(Date lastUpdated){
@@ -29,22 +35,34 @@ public class Menu{
         return items;
     }
 
+    public ArrayList<MenuItems> addItem(MenuItem item){
+        this.item.add(String.valueOf(item));
+        return this.items;
+    }
 
+    public  ArrayList<MenuItems> removeItem(MenuItems item){
+        int toBeRemoved = -1;
+        for (int i = 0; i < items.size(); i++){
+            if(items.get(i).equals(item)){
+                toBeRemoved = i;
+            }
+        }
+        this.items.remove(toBeRemoved);
+        return  this.items;
 
+    }
 
+    public  String menuUpdated(){
+        return("The menu was last updated on " + this.lastUpdated);
+    }
 
+    public  void  printItem(MenuItems item){
+        System.out.println(item.getDescription());
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+    public void printMenu(MenuItem newItem2){
+        for (int i = 0; i < items.size(); i++){
+            System.out.println(items.get(i).getDescription());
+        }
+    }
 }
--- src/org/launchcode/java/studiorestaurantmenu/Restaurant.java
+++ src/org/launchcode/java/studiorestaurantmenu/Restaurant.java
@@ -1,7 +1,56 @@
 package org.launchcode.java.studiorestaurantmenu;
 
-public class Restaurant {
-    public static void main(String[] args){
 
-    }
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+
+/*
+public class Restaurant {
+
+    public static void main(String[] args){
+        MenuItem newItem1 = new MenuItem( price: 5.55, description: "Chilly Pasta", category: "Dinner", isNew: true);
+        MenuItem newItem2 = new MenuItem( price: 8.99, description: "T-bone Steak", category: "Dinner", isNew: false);
+        MenuItem newItem3 = new MenuItem( price: 6.59, description: "Ice cream pie", category: "Dessert", isNew: true);
+
+
+        Date today = Calendar.getInstance().getTime();
+        ArrayList<MenuItems> startingMenu = new ArrayList<>();
+
+        Menu ourMenu = new Menu(today, startingMenu);
+
+        ourMenu.addItem(newItem1);
+        ourMenu.addItem(newItem2);
+        ourMenu.addItem(newItem3);
+
+        System.out.println("1. Print the entire menu to the screen.");
+        System.out.println("Code: ourMenu.printMenu();\n");
+        ourMenu.printMenu();
+
+        System.out.println("\n");
+        System.out.println("2. Print individual menu item to screen.");
+        System.out.println("Code: ourMenu.printItem(newItem2);\n");
+        ourMenu.printMenu(newItem2);
+
+        System.out.print("\n");
+        System.out.println("3. Delete an item from a menu, then reprint the menu");
+        System.out.println("ourMenu.removeItem(newItem2);");
+        System.out.println("Code: ourMenu.printMenu();\n");
+        ourMenu.printMenu(newItem2);
+        ourMenu.printMenu();
+
+        System.out.print("\n");
+        System.out.println("Printing price and category");
+        System.out.println("System.out.printf(\"%s\", newItems2.category);");
+        System.out.println("System.out.printf(\"%d\", newItem2.price);\n");
+        System.out.printf("%s \n", newItem2.description);
+        System.out.printf("%s \n", newItem2.category);
+        System.out.printf("%s \n", newItem2.price);
+
+
 }
+}
+
+
+
